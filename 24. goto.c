7f46d56ce1cc19d4a3865305cf33363ac2ac5d43
HarshitKumarOjha <harshitojha46@gmail.com>
2021-05-30 14:06:04 +0530

    add boilerplate and add goto.c

---
+++ 24. goto.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() 
+{
+
+//############################## goto program 1: ##############################
+
+    // label:
+    //     printf("inside label\n");
+    
+    // goto end;
+
+    // printf("hello\n");
+    
+    // goto label;
+
+    // end:
+    //     printf("at the end\n");
+
+// ################################ goto program 2: ###############################
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        printf("%d\n",i);
+
+    }
+    
+
+
+
+    return 0;
+}
