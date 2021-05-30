36b3de2967c16af968e62a1a911f2adf7017d9c7
HarshitKumarOjha <harshitojha46@gmail.com>
2021-05-30 16:52:59 +0530

    solve some git errors

--- 24. goto.c
+++ 24. goto.c
@@ -19,14 +19,29 @@ int main()
     //     printf("at the end\n");
 
 // ################################ goto program 2: ###############################
-
+    int num;
+    
     for (int i = 0; i < 5; i++)
     {
 
-        printf("%d\n",i);
+        printf("outer loop: %d \n",i);
 
+        for (int j = 0; j <8; j++)
+        {
+            printf("Enter a number or enter 0 to exit:\n");
+            scanf("%d", &num);
+
+            if (num==0)
+            {
+                goto end;
+            }
+            
+        }
+        
     }
     
+    end:
+            printf("ended");
 
 
 
