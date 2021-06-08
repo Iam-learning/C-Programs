159f70e8dd457c31a67d3c1fe246f7ff380b0169
HarshitKumarOjha <harshitojha46@gmail.com>
2021-06-08 10:25:16 +0530

    multipliction of two 2d arrays

---
+++ 36. multiplication of two 2d arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() 
+{
+    
+    int a[3][3],b[3][3],multi[3][3];
+
+    printf("Enter 3x3 integers for array a:\n");
+    for (int i = 0; i < 3; i++)
+        for(int j=0; j<3;j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter 3x3 integers for array b:\n");
+    for (int i = 0; i < 3; i++)
+     for(int j=0; j<3;j++)
+        scanf("%d", &b[i][j]);
+    
+    printf("multiplication of array a and b:\n");
+    for (int i = 0; i < 3; i++){
+        for(int j=0; j<3;j++){
+            multi[i][j] = a[i][j] * b[i][j];
+            printf("%d ", multi[i][j]);
+        }
+    }
+    return 0;
+}
