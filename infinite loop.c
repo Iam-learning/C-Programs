a264bee343e88c901e280913aa9b76deea688ad2
HeySkidee <harshitojha46@gmail.com>
2021-09-18 00:11:33 +0530

    infinite loop prgrm

---
+++ infinite loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+
+    for( ; ; ){
+        printf("hello");
+    }
+}
+
+// print all even numbers upto hundered
+// print the table of a given number upto 10
+// reverse order of number
+// factorial
+// 0 ka factorial 1 kaise hota h bc
