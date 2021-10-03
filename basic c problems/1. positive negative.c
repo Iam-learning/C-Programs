b22534211e49b48e2824507b82c80a5f6748e251
HeySkidee <harshitojha46@gmail.com>
2021-10-03 12:29:02 +0530

    +ve _ve program

---
+++ basic c problems/1. positive negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main (){
+
+    int n=-5;
+    
+    if (n>0) printf("positive");
+    else if (n==0) printf("neither positive nor negative");
+    else printf("negative");
+
+}
