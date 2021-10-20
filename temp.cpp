3a4af8fb96be232c942a4509f2ba5f2a67ffb5a1
HeySkidee <harshitojha46@gmail.com>
2021-10-20 20:49:44 +0530

    idk

--- temp.cpp
+++ temp.cpp
@@ -5,23 +5,20 @@ using namespace std;
 int main()
 {
     
-    int n;
-
-    cin>>n;
-    if(1<=n && n<=9){
-        if(n==1) cout<<"one";
-        else if (n==2) cout<<"two";
-        else if (n==3) cout<<"three";
-        else if (n==4) cout<<"four";
-        else if (n==5) cout<<"five";
-        else if (n==6) cout<<"six";
-        else if (n==7) cout<<"seven";
-        else if (n==8) cout<<"eight";
-        else if (n==9) cout<<"nine";
+    int a, b;
+    string represent[10] = {"zero", "one", "two", "three", "four",\
+    	"five", "six", "seven", "eight", "nine"};
+    cin >> a >> b;
+    for(int i = a; i <= b; i++) {
+        
+        if(i > 9) {
+            if(i % 2 == 0)
+                cout << "even\n";
+            else cout << "odd\n";
+        }
+        else {
+            cout << represent[i]<<endl;
+        }
     }
-    else {
-    cout<<"Greater than 9";
-    }
-
     return 0;
 }
