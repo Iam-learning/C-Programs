92d0bb896a427c113ab59d3bf46787cc7a95617d
HeySkidee <harshitojha46@gmail.com>
2021-10-28 20:58:37 +0530

    idk

--- temp.cpp
+++ temp.cpp
@@ -5,20 +5,8 @@ using namespace std;
 int main()
 {
     
-    int a, b;
-    string represent[10] = {"zero", "one", "two", "three", "four",\
-    	"five", "six", "seven", "eight", "nine"};
-    cin >> a >> b;
-    for(int i = a; i <= b; i++) {
-        
-        if(i > 9) {
-            if(i % 2 == 0)
-                cout << "even\n";
-            else cout << "odd\n";
-        }
-        else {
-            cout << represent[i]<<endl;
-        }
-    }
+    int a=10,X;
+    X= a-- + ++a;
+    printf("%d",X);
     return 0;
 }
